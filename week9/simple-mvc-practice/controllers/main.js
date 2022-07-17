150793b25aecf997cc6263beecb345797722fad3
zlongboy <zlongboy@me.com>
2022-07-17 12:38:20 -0400

    get rendering dynamic content working -- fix issue with var names

--- week9/simple-mvc-practice/controllers/main.js
+++ week9/simple-mvc-practice/controllers/main.js
@@ -1,5 +1,4 @@
 //import model
-
 import { __dirname, __filename } from '../app.js'
 
 export let records = [];
@@ -13,7 +12,6 @@ export const getAddRecords = (req, res) => {
 
 export const postAddRecords = (req, res) => {
     records.push({ title: req.body.name });
-    console.log(records);
     res.redirect('/');
 };
 
