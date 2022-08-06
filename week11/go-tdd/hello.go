d50f1715b9e8bf2c882e13011d50d0b1e424201f
zlongboy <zlongboy@me.com>
2022-08-05 23:23:22 -0400

    multiple refactored tests on hello world func

--- week11/go-tdd/hello.go
+++ week11/go-tdd/hello.go
@@ -4,10 +4,33 @@ import (
 	"fmt"
 )
 
-func Hello(n string) string {
-	return "Hello, " + n
+const engGreeting = "Hello, "
+const spaGreeting = "Hola, "
+const freGreeting = "Bonjour, "
+
+const spanish = "Spanish"
+const french = "French"
+
+func Hello(n string, lang string) string {
+	if n == "" {
+		n = "world"
+	}
+
+	return helloPrefix(lang) + n
+}
+
+func helloPrefix(lang string) (greet string) {
+	switch lang {
+	case spanish:
+		greet = spaGreeting
+	case french:
+		greet = freGreeting
+	default:
+		greet = engGreeting
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("Zack"))
+	fmt.Println(Hello("Zack", spanish))
 }
--- week11/go-tdd/hello_test.go
+++ week11/go-tdd/hello_test.go
@@ -5,9 +5,31 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	got := Hello("Zack")
-	want := "Hello, Zack"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Zack", "")
+		want := "Hello, Zack"
+		errorMsg(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Zack", "Spanish")
+		want := "Hola, Zack"
+		errorMsg(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Zack", "French")
+		want := "Bonjour, Zack"
+		errorMsg(t, got, want)
+	})
+	t.Run("say 'Hello, world' when empty string is passed in", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, world"
+		errorMsg(t, got, want)
+	})
+}
 
+// Testing helpers
+func errorMsg(t testing.TB, got, want string) { // testing.TB is an interface for testing.T and testing.B
+	t.Helper() // Indicates this function is a helper and directs failure log to line of actual failure (function call)
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
